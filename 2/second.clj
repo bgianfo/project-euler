8cbd0db72c6f7ea4a5dbbb0e1fb2d1f0447ea8b8
Brian Gianforcaro <b.gianfo@gmail.com>
2009-03-01 00:41:22 -0500

    prob 2 initial solution, no workie

---
+++ 2/second.clj
@@ -0,0 +1,12 @@
+;Project Euler Problem #2
+;Initial solution, not working yet
+
+(def four-million 4000000)
+
+(def fib-seq
+     (lazy-cat [0 1] (map + (rest fib-seq) fib-seq)))
+
+(println (apply + (filter #(or (even? %)(<= % four-million))
+                    (take 500 fib-seq)))) 
+
+
