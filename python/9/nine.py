154216afa3b383ea9a64857bca1d0a8a03f8f256
Brian Gianforcaro <b.gianfo@gmail.com>
2014-11-29 21:57:32 -0800

    Fix message and break in old solution to nine.

--- python/9/nine.py
+++ python/9/nine.py
@@ -6,4 +6,5 @@ for a in range(1,1000):
         if a**2+ b**2 == c**2:
           if (a+b+c) == 1000:
             print "a: %s b: %s c: %s " % (a, b ,c)
-            print "sum: ", a*b*c
+            print "product: ", a*b*c
+			break
